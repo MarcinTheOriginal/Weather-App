838dadb84b09d304d72c03ffbbfc6c5e06c418aa
MarcinTheOriginal <mmanicki@gmail.com>
2021-05-31 20:52:05 +0200

    Pobieranie temperatur

--- main.js
+++ main.js
@@ -7,12 +7,27 @@ const photo = document.querySelector('.photo');
 
 const weather = document.querySelector('.weather');
 const temperature = document.querySelector('.temp');
-const humidity = document.querySelector('.humidity');
+const feels = document.querySelector('.feels');
 
 const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const apiKey = '&appid=976496ddd59a03609dd2d3e707c4c831';
 const apiUnits = '&units=metric';
 
-let city;
-let url;
+let $city = 'konin';
+let $url;
 
+const getWeather = () => {
+    $url = apiLink + $city + apiKey + apiUnits;
+
+    axios.get($url).then(response => {
+        console.log(response.data.main);
+        const temp = Math.round(response.data.main.temp);
+        const tempFeels = Math.round(response.data.main.feels_like);
+
+        cityName.textContent = response.data.name;
+        temperature.textContent = temp + '°C';
+        feels.textContent = tempFeels + '°C';
+    });
+}
+
+getWeather();
