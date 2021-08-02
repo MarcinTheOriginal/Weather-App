f972f7c66bcc8fbeb628b68860e2d410265566a6
MarcinTheOriginal <mmanicki@gmail.com>
2021-08-02 18:10:12 +0200

    Usunięcie api key

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const temperature = document.querySelector('.temp');
 const feels = document.querySelector('.feels');
 
 const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=';
-const apiKey = '&appid=976496ddd59a03609dd2d3e707c4c831';
+const apiKey = '&appid=';
 const apiUnits = '&units=metric';
 
 const $defaultCity = 'Konin';
